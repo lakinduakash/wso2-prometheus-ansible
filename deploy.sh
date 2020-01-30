f632256f1d0b3e6229e3c9744ba7926fd9b3610d
Lakindu Akash <lakinduakash@gmail.com>
2020-01-30 15:47:02 +0530

    Remove -f flag install roles Add -vv flag ansible-play

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 export ANSIBLE_FORCE_COLOR=true
 
-ansible-galaxy install -f -r roles/requirements.yml
+ansible-galaxy install -r roles/requirements.yml
 
 echo "Creating Vault-file: /tmp/vault"
 /bin/cat <<EOM >/tmp/vault
 ${ANSIBLE_VAULT_PASSWORD}
 EOM
 
-ansible-playbook --vault-id /tmp/vault site.yml
+ansible-playbook --vault-id /tmp/vault site.yml -vv
 EXIT_CODE=$?
 
 shred /tmp/vault
